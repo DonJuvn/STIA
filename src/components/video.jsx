39c6364ca7acab045ccca8a06adb31830637274a
DonJuan <armvnov1ch@gmail.com>
2024-12-14 02:55:42 +0500

    video adaptive

---
+++ src/components/video.jsx
@@ -0,0 +1,16 @@
+import React from "react";const Video = () => {
+   return (
+      <div id="video">
+         <div className="video">
+            <img src="img/bg.png" alt="" />
+            <h1>
+               ТОО “Стройинжиниринг Астана” Ведущая компания Республики
+               Казахстан по стандартизации и нормированию в нефтегазовой отрасли
+            </h1>
+         </div>
+         <div className="filter"></div>
+      </div>
+   );
+};
+
+export default Video;
