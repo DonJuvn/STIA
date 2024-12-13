e1039a3c1a2190cb07ba2266060e238ce9564bc9
DonJuan <armvnov1ch@gmail.com>
2024-12-14 03:05:02 +0500

    about

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Menu from "./components/menu";
 import Video from "./components/video";
+import About from "./components/about";
 
 function App() {
    return (
       <>
          <Menu />
          <Video />
+         <About />
       </>
    );
 }
