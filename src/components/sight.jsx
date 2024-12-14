7f51734343fbc5e833de0edc7f584be9bbdbdea9
DonJuan <armvnov1ch@gmail.com>
2024-12-14 20:42:21 +0500

    sight

---
+++ src/components/sight.jsx
@@ -0,0 +1,25 @@
+import React from "react";const Sight = () => {
+   return (
+      <div id="sight">
+         <div className="container">
+            <div className="title">
+               <div className="dot"></div>
+               <h1 id="title">Видение</h1>
+            </div>
+            <div className="history">
+               <p>
+                  Стать ключевым элементом в развитии промышленности,
+                  устанавливая стандарты качества, безопасности и устойчивого
+                  развития. Мы стремимся быть предпочтительным выбором для
+                  предприятий Казахстана и стран СНГ, известных своей
+                  надежностью и приверженностью качеству в каждой области нашей
+                  деятельности.
+               </p>
+               <h1 id="number">(02)</h1>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Sight;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import History from "../components/history";
+import Sight from "../components/sight";
 
 const AboutPage = () => {
    return (
       <>
       <History />
+      <Sight />
       </>
    );
 };
