42e9f3514e3df10c897fc36c9be1c8fe73b39e4f
DonJuan <armvnov1ch@gmail.com>
2024-12-15 00:08:14 +0500

    licenses

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import Sight from "../components/sight";
 import Mission from "../components/mission";
 import Goals from "../components/goals";
 import Goals2 from "../components/goals2";
+import Licenses from "../components/licenses";
 
 const AboutPage = () => {
    return (
@@ -13,6 +14,7 @@ const AboutPage = () => {
          <Mission />
          <Goals />
          <Goals2 />
+         <Licenses />
       </>
    );
 };
