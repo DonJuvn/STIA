1cea9cbb99283356266f39386eaf2bb197bc0880
DonJuan <armvnov1ch@gmail.com>
2024-12-17 21:04:52 +0500

    video added

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,8 +1,12 @@
-import React from "react";const Video = () => {
+import React from "react";
+const Video = () => {
    return (
       <div id="video">
          <div className="video">
-            <img src="img/bg.png" alt="" />
+            <video autoPlay muted loop playsInline>
+               <source src="img/bg.mp4" type="video/mp4" />
+               Your browser does not support the video tag.
+            </video>
             <h1>
                ТОО “Стройинжиниринг Астана” Ведущая компания Республики
                Казахстан по стандартизации и нормированию в нефтегазовой отрасли
